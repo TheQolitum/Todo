2fd272b8a0579b70ec67b0c2f8ce3a44912887bc
TheQolitum <qolitum@gmail.com>
2022-09-12 19:23:12 +0700

    Main

---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <body>
+  <div class="container">
+    <div class="header">
+      <h1>Not Microsoft To Do</h1>
+      <div class="login-route">
+        <h2>
+          <router-link to="/login">Log in</router-link>
+        </h2>
+      </div>
+    </div>
+    <div id="app">
+      <div class="content">
+        <div class="input">
+          <h2>Введите текст задачи:</h2>
+          <input id="task-input" type="text" v-model="item" @keypress.enter="add">
+          <button @click="add">Добавить</button>
+        </div>
+        <h2>Надо сделать:</h2>
+        <ul class="task-list" v-if="itemsExist">
+          <li class="task" v-for="(item, index) in items" :key="index">
+            <button @click="remove(index)">Сделано</button>
+            <textarea rows="6">{{ item }}</textarea>
+          </li>
+        </ul>
+        <div v-else>
+          Ура! Всё сделано!
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+      item: ''
+    }
+  },
+  methods: {
+    add(item) {
+      this.items.push(this.item);
+    },
+    remove(index, items) {
+      this.items.splice(index, 1);
+    },
+  },
+  computed: {
+    itemsExist() {
+      return this.items.length != 0;
+    }
+  }
+}
+</script>
+
+<style lang="css">
+@import "assets/main.css";
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <body id="app__login">
+  <div class="container">
+    <h1>Добро пожаловать!</h1>
+    <div class="login__body">
+      <p class="login__attribute">Введите email:</p>
+      <input class="log" type="text" v-model="login">
+      <p class="err" v-if="showLoginError">Вы не ввели логин!</p>
+      <p class="login__attribute">Введите пароль:</p>
+      <input class="login__attribute" type="password" v-model="password">
+      <p class="err" v-if="showPassError">Вы не ввели пароль!</p>
+      <br>
+      <button @click="checker">
+        Log in
+      </button>
+    </div>
+  </div>
+  </body>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: '',
+      showLoginError: false,
+      showPassError: false
+    }
+  },
+  methods: {
+    checker() {
+      if (this.validateLogin() & this.validatePass()) {
+        alert('Good');
+      }
+    },
+    validateLogin() {
+      let loginExist = !!this.login;
+      this.showLoginError = !loginExist;
+      return loginExist;
+    },
+    validatePass() {
+      let passExist = !!this.password;
+      this.showPassError = !passExist;
+      return passExist;
+    },
+  }
+}
+</script>
+
+<style lang="css">
+@import "assets/login.css";
+</style>
